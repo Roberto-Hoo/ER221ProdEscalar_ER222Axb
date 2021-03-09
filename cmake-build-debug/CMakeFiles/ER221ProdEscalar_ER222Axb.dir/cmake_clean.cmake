cfd79dd517531d8a762317cb95541e8801c3e880
Roberto Hoo <robertohoo@gmail.com>
2021-03-09 10:14:34 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ER221ProdEscalar_ER222Axb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ER221ProdEscalar_ER222Axb.dir/GSL_ProdEscalar_Axb.cpp.obj"
+  "ER221ProdEscalar_ER222Axb.exe"
+  "ER221ProdEscalar_ER222Axb.exe.manifest"
+  "ER221ProdEscalar_ER222Axb.pdb"
+  "libER221ProdEscalar_ER222Axb.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ER221ProdEscalar_ER222Axb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
